3a9f285b439153bb7115dec740ccb1efef0068d9
rianbar <rianbarroso596@gmail.com>
2024-09-25 23:39:47 -0300

    exercise 12 bug fixed

--- src/com/rian/example/Exercise12.java
+++ src/com/rian/example/Exercise12.java
@@ -5,7 +5,7 @@ import java.util.stream.LongStream;
 public class Exercise12 {
     public static void main(String[] args) {
         long result = LongStream.iterate(1, n -> n + 1)
-            .map(n -> LongStream.rangeClosed(1, n).sum())
+            .map(n -> n * (n + 1) / 2)
             .filter(sn -> divisorsVerify(sn) > 500)
             .findFirst().orElse(0);
         
